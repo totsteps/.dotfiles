a8c48268fb2baf24e500ad311f5b8c39fb23cf30
Gagandeep Singh <totsteps.gs@gmail.com>
2022-09-15 18:11:08 +0200

    Update boostrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
-# create ~/just
+# download and make 'just' available as a shell command
 mkdir -p ~/just
-
-# download and extract just to ~/just/just
 curl --proto "=https" --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ~/just
-
-# add `~/just` to the paths
 export PATH="$PATH:$HOME/just"
 
-# NIX_PATH for home-manager
+# NIX_PATH for home-manager; 'just' seems to have issues with exporting path variables
 export NIX_PATH="$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}"
 
-# just
 just nix
 
 # resource shell
